2d7451b5480efd721a0c2177dfaa58669be41e6c
Mamix84 <38494340+Mamix84@users.noreply.github.com>
2018-05-18 14:32:34 +0200

    Added new function
    Added new function

--- index.js
+++ index.js
@@ -20,3 +20,44 @@ exports.squareArea = function(side_a) {
 exports.squarePerimeter = function(side_a) {
 	return side_a * 4;
 }
+
+/**
+ * Calculate the are of a rectangle
+ * 
+ * @param {*} length 
+ * @param {*} width 
+ */
+exports.rectangleArea = function(length, width){
+	return length * width;
+}
+
+/**
+ * Calculate the perimeter of a rectangle
+ * 
+ * @param {*} length 
+ * @param {*} width 
+ */
+exports.rectanglePerimeter = function(length, width){
+	return (2 * length) + (2 * width);
+}
+
+/**
+ * Calculate the are of a triangle
+ * 
+ * @param {*} base 
+ * @param {*} altitutde 
+ */
+exports.triangleArea = function(base, altitutde){
+	return (base * altitutde) / 2;
+}
+
+/**
+ * Calculate the perimeter of a triangle
+ * 
+ * @param {*} side_1 
+ * @param {*} side_2
+ * @param {*} side_3
+ */
+exports.trianglePerimeter = function(side_1,side_2,side_3){
+	return side_1 + side_2 + side_3;
+}
--- tests/test.js
+++ tests/test.js
@@ -14,3 +14,36 @@ test("geometry.squarePerimeter", function(assert) {
     assert.equals(geometry.squarePerimeter(a), 20)
     assert.end()
 })
+
+test("geometry.rectangleArea", function(assert) {
+    var a = 5
+    var b = 10
+
+    assert.equals(geometry.rectangleArea(a,b), 50)
+    assert.end()
+})
+
+test("geometry.rectanglePerimeter", function(assert) {
+    var a = 5
+    var b = 10
+
+    assert.equals(geometry.rectanglePerimeter(a,b), 30)
+    assert.end()
+})
+
+test("geometry.triangleArea", function(assert) {
+    var a = 5
+    var b = 10
+
+    assert.equals(geometry.triangleArea(a,b), 25)
+    assert.end()
+})
+
+test("geometry.trianglePerimeter", function(assert) {
+    var a = 5
+    var b = 10
+    var c = 15
+
+    assert.equals(geometry.trianglePerimeter(a,b,c), 30)
+    assert.end()
+})
